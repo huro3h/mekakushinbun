c0cca41f36c338bcfc0d1b9576862786622dee77
huro3h_satoshi <huro3h@gmail.com>
2016-06-17 15:45:50 +0800

    ライブラリ SwiftyJSON 導入

--- mekakushinbun/ViewController.swift
+++ mekakushinbun/ViewController.swift
@@ -5,9 +5,10 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UITableViewDataSource,
-UITableViewDelegate, UIScrollViewDelegate {
+UITableViewDelegate, UIScrollViewDelegate, NSXMLParserDelegate {
 
 	@IBOutlet weak var listTableView: UITableView!
 	
@@ -19,20 +20,19 @@ UITableViewDelegate, UIScrollViewDelegate {
 		Alamofire.request(.GET, "http://news.yahoo.co.jp/pickup/rss.xml", parameters: nil)
 			.response { (request, response, data, error) in
 				print ("requestの中身\(request)")
-				print("responseの中身\(response)")
-				print("dataの中身\(data)")
-				print("errorの中身\(error)")
+				print ("responseの中身\(response)")
+				print ("dataの中身\(data)")
+				print ("errorの中身\(error)")
 		}
+		
+		// XML解析実行
+		//self.loadxml()
 	}
 	
 	override func viewWillAppear(animated: Bool) {
 		print("news一覧画面表示")
 		listTableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
 	}
-
-//	func tableView(tableView: UITableView,canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-//		return true
-//	}
 	
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return 10 // 行数をデータの数でカウント
@@ -66,7 +66,54 @@ UITableViewDelegate, UIScrollViewDelegate {
 		return dateString
 	}
 	
+	// XMLを解析する
+//	func loadxml(){
+//		let url_text = "http://news.yahoo.co.jp/pickup/rss.xml"
+//		
+//		guard let url = NSURL(string: url_text) else{
+//			return
+//		}
+//		
+//		// インターネット上のXMLを取得し、NSXMLParserに読み込む
+//		guard let parser = NSXMLParser(contentsOfURL: url) else{
+//			return
+//		}
+//		parser.delegate = self;
+//		parser.parse()
+//	}
 	
+//	// XML解析開始時に実行されるメソッド
+//	func parserDidStartDocument(parser: NSXMLParser) {
+//		print("XML解析開始しました")
+//	}
+//	
+//	// 解析中に要素の開始タグがあったときに実行されるメソッド
+//	func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+//		print("開始タグ:" + elementName)
+//	}
+//	
+//	// 開始タグと終了タグでくくられたデータがあったときに実行されるメソッド
+//	func parser(parser: NSXMLParser, foundCharacters string: String) {
+//		print("要素:" + string)
+//	}
+//	
+//	// 解析中に要素の終了タグがあったときに実行されるメソッド
+//	func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+//		print("終了タグ:" + elementName)
+//	}
+//	
+//	// XML解析終了時に実行されるメソッド
+//	func parserDidEndDocument(parser: NSXMLParser) {
+//		print("XML解析終了しました")
+//	}
+//	
+//	// 解析中にエラーが発生した時に実行されるメソッド
+//	func parser(parser: NSXMLParser, parseErrorOccurred parseError: NSError) {
+//		print("エラー:" + parseError.localizedDescription)
+//	}
+
+
+
 //	override func didReceiveMemoryWarning() {
 //		super.didReceiveMemoryWarning()
 //	}
---
+++ mekakushinbun/dummy.m
@@ -0,0 +1,9 @@
+//
+//  dummy.m
+//  mekakushinbun
+//
+//  Created by satoshiii on 2016/06/17.
+//  Copyright © 2016年 satoshiii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
