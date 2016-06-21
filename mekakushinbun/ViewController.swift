df5f5ab837de005588e37be3d0efe6d8b687c300
huro3h_satoshi <huro3h@gmail.com>
2016-06-21 14:35:20 +0800

    変換したJSONデータ取得 4階層くらい掘り下げて...なにこれ(´・ω・`)

--- mekakushinbun/ViewController.swift
+++ mekakushinbun/ViewController.swift
@@ -18,13 +18,12 @@ UITableViewDelegate, UIScrollViewDelegate, NSXMLParserDelegate {
 		super.viewDidLoad()
 		// 別でcellファイルを作った時に
 		listTableView.registerNib(UINib(nibName: "newsCell", bundle: nil), forCellReuseIdentifier: "newsCell")
-		
 		getArticles()
 		
 	}
 
 	override func viewWillAppear(animated: Bool) {
-		print("news一覧画面表示")
+//		print("news一覧画面表示")
 		
 //		listTableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
 	}
@@ -38,7 +37,7 @@ UITableViewDelegate, UIScrollViewDelegate, NSXMLParserDelegate {
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCellWithIdentifier("newsCell")! as! newsTableViewCell
 		let article = articles[indexPath.row] // 行数番目の記事を取得
-//		print("なにがはいってるか表示\(article)")
+		print("なにがはいってるか表示\(article)")
 		cell.subjectLabel?.text = article["title"]!
 		cell.dateTimeLabel.text = article["publishedDate"]!
 //		cell.detailLabel.text = "詳細\([indexPath.row])"
@@ -64,26 +63,36 @@ UITableViewDelegate, UIScrollViewDelegate, NSXMLParserDelegate {
 	
 	func getArticles() {
 		Alamofire.request(.GET, "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://news.yahoo.co.jp/pickup/rss.xml&num=8") // APIへリクエストを送信
-			.responseJSON { response in
-				// ここに処理を記述していく
-				guard let object = response.result.value else {
-					return
-				//print(response.result.value)
-				}
-					let json = JSON(object)
-					json.forEach { (_, json) in
-						let article: [String: String?] = [
-						"title": json["title"].string,
-						"publishedDate" :json["publishedDate"].string
-						]
-						self.articles.append(article)
-						//print(json["title"].string)
-					}
-				print(self.articles)
-				// 非同期通信の為、上のほうで１回目カウントした際は空振りしている
-				// それ故、読み直しさせることで正常に表示させる
-				self.listTableView.reloadData()
+		.responseJSON { response in
+			// ここに処理を記述していく
+			guard let object = response.result.value else {
+				return
+			//print(response.result.value)
 			}
+				let json = JSON(object)
+				json.forEach { (key, json) in
+					if(key == "responseData") {
+						json.forEach { (key, json) in
+							json.forEach { (key, jsonArray) in
+								if(key == "entries") {
+									jsonArray.forEach{ (_, json) in
+										let article: [String: String?] = [
+											"title": json["title"].string,
+											"publishedDate" : json["publishedDate"].string
+										]
+										self.articles.append(article)
+									}
+								}
+							}
+						}
+					}
+				}
+			
+			print(self.articles)
+			// 非同期通信の為、上のほうで１回目カウントした際は空振りしている
+			// ので、読み直しさせることで正常に表示させる
+			self.listTableView.reloadData()
+		}
 	}
 	
 	
