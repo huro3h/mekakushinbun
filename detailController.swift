444ef6b4f3da1738fb0b6ba3ff0ffd68abac869a
huro3h_satoshi <huro3h@gmail.com>
2016-06-23 15:01:02 +0800

    detailViewにニュース内容表示させるところまで

--- detailController.swift
+++ detailController.swift
@@ -4,20 +4,45 @@
 //  Copyright © 2016年 satoshiii. All rights reserved.
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class detailController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var myWebView: UIWebView!
+	@IBOutlet weak var mytextView: UITextView!
+	@IBOutlet weak var webtextSwitch: UISegmentedControl!
+
+	
+	var selectedIndex = -1
+	var newsUrl: String = ""
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
+		print(newsUrl)
     }
 	
 	override func viewWillAppear(animated: Bool) {
-		print("詳細画面表示")
+		print("詳細画面表示\(selectedIndex)")
+		let myURL = NSURL(string: newsUrl)
+		let myURLReq = NSURLRequest(URL: myURL!)
+		myWebView.loadRequest(myURLReq)
+	}
+	
+	@IBAction func pushSegmented(sender: UISegmentedControl) {
+		
+		switch sender.selectedSegmentIndex {
+			case 0:
+				mytextView.hidden = true
+				myWebView.hidden = false
+			case 1:
+				mytextView.hidden = false
+				myWebView.hidden = true
+			default:
+				print("該当無し")
+		}
 	}
 
-	
-	
-	
 
 	
 	
--- mekakushinbun/ViewController.swift
+++ mekakushinbun/ViewController.swift
@@ -12,6 +12,8 @@ UITableViewDelegate, UIScrollViewDelegate, NSXMLParserDelegate {
 
 	@IBOutlet weak var listTableView: UITableView!
 	
+	var selectedIndex = -1
+	
 	var newsTopicTypes: [String] =
 		["http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://news.yahoo.co.jp/pickup/rss.xml&num=15",
 		 "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://news.yahoo.co.jp/pickup/world/rss.xml&num=15",
@@ -19,7 +21,6 @@ UITableViewDelegate, UIScrollViewDelegate, NSXMLParserDelegate {
 		 "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://news.yahoo.co.jp/pickup/science/rss.xml&num=15"]
 	
 	var newsCategoryNames: [String] = ["トップ","国際","経済","サイエンス"]
-	
 	var articles: [[String: AnyObject?]] = [] // 記事を入れるプロパティを定義
 
 	override func viewDidLoad() {
@@ -28,9 +29,12 @@ UITableViewDelegate, UIScrollViewDelegate, NSXMLParserDelegate {
 		listTableView.registerNib(UINib(nibName: "newsCell", bundle: nil), forCellReuseIdentifier: "newsCell")
 		getArticles()
 	}
+	
+	
+//	override func viewWillAppear(animated: Bool) {
+//	}
+	
 
-	override func viewWillAppear(animated: Bool) {
-	}
 	
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return articles.count // 行数をデータの数でカウント
@@ -47,8 +51,19 @@ UITableViewDelegate, UIScrollViewDelegate, NSXMLParserDelegate {
 		return cell
 	}
 	
-	func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		print("\(indexPath.row)行目を選択")
+		selectedIndex = indexPath.row
+		performSegueWithIdentifier("detailSegue", sender: nil)
 	}
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		let detailVC = segue.destinationViewController as! detailController
+		detailVC.selectedIndex = selectedIndex
+		detailVC.newsUrl = articles[selectedIndex]["link"] as! String
+		
+	}
+	
 	
 	
 	// 自作関数置き場
@@ -75,6 +90,7 @@ UITableViewDelegate, UIScrollViewDelegate, NSXMLParserDelegate {
 	func getArticles() {
 		
 		var newsCount: Int = 1
+		let newsCategoryNamesCounter: Int = newsCategoryNames.count
 		
 		for a in newsTopicTypes {
 			Alamofire.request(.GET, "\(a)") // APIへリクエストを送信
@@ -111,10 +127,10 @@ UITableViewDelegate, UIScrollViewDelegate, NSXMLParserDelegate {
 				// ので、読み直しさせることで正常に表示させる
 				self.listTableView.reloadData()
 				
-				if (newsCount == 4){
+				if (newsCount == newsCategoryNamesCounter){
 					self.articles = self.articles.sort{($0["publishedDate"] as! NSDate).compare($1["publishedDate"] as! NSDate) == NSComparisonResult.OrderedDescending
 					}
-					print(self.articles)
+					// print(self.articles)
 				}
 				newsCount++
 			}
